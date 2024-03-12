cbe9353ed6d220c6329acfc7021e71ffb4a4c3b7
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-03-12 16:24:34 +0200

    change layout , update the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'firebase_options.dart'; // Ensure this file is correctly linked to your Firebase project.
+import 'firebase_options.dart'; // Make sure this is correctly set up for your Firebase project.
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,34 +30,34 @@ class PricesDisplayPage extends StatefulWidget {
 
 class _PricesDisplayPageState extends State<PricesDisplayPage> {
   String selectedCurrency = 'USD';
+  String selectedMetal = 'XAU'; // XAU for Gold, XAG for Silver
+  List<String> metals = ['XAU', 'XAG'];
   List<String> currencies = ['USD', 'AUD', 'GBP', 'EUR', 'CHF', 'CAD', 'JPY', 'EGP', 'KWD', 'SAR'];
+  Map<String, TextEditingController> caratControllers = {};
+  final String apiKey = 'goldapi-1lsltm3aowc-io';
   TextEditingController _newCurrencyController = TextEditingController();
-  TextEditingController _goldPriceController = TextEditingController();
-  TextEditingController _silverPriceController = TextEditingController();
-  final String apiKey = 'goldapi-1lsltm3aowc-io'; // Replace with your actual API key.
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPrices();
+  }
 
   Future<void> fetchPrices() async {
-    if (!currencies.contains(_newCurrencyController.text.toUpperCase()) && _newCurrencyController.text.isNotEmpty) {
-      setState(() {
-        currencies.add(_newCurrencyController.text.toUpperCase());
-        selectedCurrency = _newCurrencyController.text.toUpperCase();
-      });
-    }
-
-    String urlGold = 'https://www.goldapi.io/api/XAU/$selectedCurrency';
-    String urlSilver = 'https://www.goldapi.io/api/XAG/$selectedCurrency';
+    final String url = 'https://www.goldapi.io/api/$selectedMetal/$selectedCurrency';
     try {
-      var responseGold = await http.get(Uri.parse(urlGold), headers: {"x-access-token": apiKey});
-      var responseSilver = await http.get(Uri.parse(urlSilver), headers: {"x-access-token": apiKey});
-      if (responseGold.statusCode == 200 && responseSilver.statusCode == 200) {
-        var dataGold = json.decode(responseGold.body);
-        var dataSilver = json.decode(responseSilver.body);
-        if (dataGold['price_gram_24k'] == null || dataSilver['price_gram_24k'] == null) {
-          showAlert('Please enter the prices manually.');
-        } else {
-          _goldPriceController.text = dataGold['price_gram_24k'].toString();
-          _silverPriceController.text = dataSilver['price_gram_24k'].toString();
-        }
+      final response = await http.get(Uri.parse(url), headers: {"x-access-token": apiKey});
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          caratControllers = {
+            '24k': TextEditingController(text: data['price_gram_24k'].toString()),
+            '22k': TextEditingController(text: data['price_gram_22k'].toString()),
+            '21k': TextEditingController(text: data['price_gram_21k'].toString()),
+            '20k': TextEditingController(text: data['price_gram_20k'].toString()),
+            '18k': TextEditingController(text: data['price_gram_18k'].toString()),
+          };
+        });
       } else {
         showAlert('Failed to fetch prices. Please enter the prices manually.');
       }
@@ -67,27 +67,42 @@ class _PricesDisplayPageState extends State<PricesDisplayPage> {
   }
 
   void uploadPricesToFirestore() async {
-    if (_goldPriceController.text.isEmpty || _silverPriceController.text.isEmpty) {
-      showAlert('Please enter prices for both gold and silver before uploading.');
+    if (caratControllers.entries.any((element) => element.value.text.isEmpty)) {
+      showAlert('Please enter prices for all carats before uploading.');
       return;
     }
 
-    double goldPrice = double.tryParse(_goldPriceController.text) ?? 0.0;
-    double silverPrice = double.tryParse(_silverPriceController.text) ?? 0.0;
+    final Map<String, String> metalNames = {
+      'XAU': 'Gold',
+      'XAG': 'Silver',
+    };
+
+    Map<String, dynamic> metalPrices = {};
+    caratControllers.forEach((carat, controller) {
+      metalPrices[carat] = double.tryParse(controller.text) ?? 0.0;
+    });
+
     final documentReference = FirebaseFirestore.instance.collection('metalPrices').doc(selectedCurrency);
+
+    Map<String, dynamic> updateData = {
+      'currency': selectedCurrency,
+      'last_updated': FieldValue.serverTimestamp(),
+      'metals': {
+        metalNames[selectedMetal] ?? 'Unknown': metalPrices, // Use the full metal name
+      }
+    };
 
     try {
       await documentReference.set({
-        'currency': selectedCurrency,
-        'price_of_gold': goldPrice,
-        'price_of_silver': silverPrice,
-        'last_updated': FieldValue.serverTimestamp(),
-      });
+        ...updateData,
+        'metals': FieldValue.arrayUnion([updateData['metals']])
+      }, SetOptions(merge: true));
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Prices updated successfully in Firestore.')));
     } catch (e) {
       showAlert('Error updating prices in Firestore: $e');
     }
   }
+
 
   void showAlert(String message) {
     showDialog(
@@ -118,51 +133,74 @@ class _PricesDisplayPageState extends State<PricesDisplayPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: DropdownButtonFormField<String>(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  filled: true,
-                  fillColor: Colors.white,
-                  labelText: 'Choose Your Currency',
-                ),
-                value: selectedCurrency,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    selectedCurrency = newValue!;
-                    fetchPrices();
-                  });
-                },
-                items: currencies.map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(labelText: 'Select Metal'),
+              value: selectedMetal,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedMetal = newValue!;
+                  fetchPrices();
+                });
+              },
+              items: metals.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value == 'XAU' ? 'Gold' : 'Silver'),
+                );
+              }).toList(),
+            ),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(labelText: 'Choose Your Currency'),
+              value: selectedCurrency,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedCurrency = newValue!;
+                  fetchPrices();
+                });
+              },
+              items: currencies.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
             TextField(
               controller: _newCurrencyController,
-              decoration: InputDecoration(labelText: 'Add/Select New Currency'),
+              decoration: InputDecoration(
+                labelText: 'Add New Currency',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    final newCurrency = _newCurrencyController.text.toUpperCase();
+                    if (!currencies.contains(newCurrency) && newCurrency.isNotEmpty) {
+                      setState(() {
+                        currencies.add(newCurrency);
+                        selectedCurrency = newCurrency;
+                        _newCurrencyController.clear();
+                        fetchPrices();
+                      });
+                    }
+                  },
+                ),
+              ),
             ),
-            ElevatedButton(
-              onPressed: fetchPrices,
-              child: Text('Fetch Prices'),
-            ),
-            TextField(
-              controller: _goldPriceController,
-              decoration: InputDecoration(labelText: 'Gold Price Per Gram'),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              controller: _silverPriceController,
-              decoration: InputDecoration(labelText: 'Silver Price Per Gram'),
-              keyboardType: TextInputType.number,
-            ),
-            ElevatedButton(
-              onPressed: uploadPricesToFirestore,
-              child: Text('Upload Edited Prices to Firestore'),
+            ...caratControllers.entries.map((entry) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: TextField(
+                  controller: entry.value,
+                  decoration: InputDecoration(labelText: '${entry.key} Price Per Gram'),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                ),
+              );
+            }).toList(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: ElevatedButton(
+                onPressed: uploadPricesToFirestore,
+                child: Text('Upload Edited Prices to Firestore'),
+              ),
             ),
           ],
         ),
